2081c53d7f1f4262ea8b1aac43ba613a1192a276
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-17 00:14:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req, res){
-    res.send('article one is requested and will be here soon');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-three',function (req, res){
     res.send('article three is requested and will be here soon');
