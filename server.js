806905aae8af2f88ca99bb21385c0a1bed7a54f9
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-18 15:37:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('//best-background-pictures-for-laptop-hd-wallpaper.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '/best-background-pictures-for-laptop-hd-wallpaper.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'best-background-pictures-for-laptop-hd-wallpaper.jpg'));
 });
 app.get('/article-one',function (req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
