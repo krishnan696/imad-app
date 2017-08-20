38a1673343751b85cde14f68bafb31f44c4e7f18
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-21 01:10:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ function createTemplate(data){
 
 
 app.get('/', function (req, res) {
-  res.sendt-File(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var count=0;
 app.get('/counter',function(req,res){
