8d6004189a2eb4cddc4a250d4507f55b2630c2cb
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-20 19:33:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,8 +136,12 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/ui/best.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'best.jpg'));
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 app.get('/:articleName',function (req, res){
     var articleName = req.params.articleName;
