31c8e2375c969bbcbb5fede02a19ad77423a81e4
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-20 16:29:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,89 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne=
+{
+    title : 'articleone|krishnan',
+    heading: 'articleone',
+    date: '20 aug 2017',
+    content:`
+    
+        <p>
+            this is the para
+            Java software for your computer, or the Java Runtime Environment, is also referred to as the Java Runtime, Runtime Environment, Runtime, JRE, Java Virtual Machine, Virtual Machine, Java VM, JVM, VM, Java plug-in, Java plugin, Java add-on or Java download
+        </p>
+    
+        <p>
+            this is the para
+            Java software for your computer, or the Java Runtime Environment, is also referred to as the Java Runtime, Runtime Environment, Runtime, JRE, Java Virtual Machine, Virtual Machine, Java VM, JVM, VM, Java plug-in, Java plugin, Java add-on or Java download
+        </p>
+    
+        <p>
+            this is the para
+            Java software for your computer, or the Java Runtime Environment, is also referred to as the Java Runtime, Runtime Environment, Runtime, JRE, Java Virtual Machine, Virtual Machine, Java VM, JVM, VM, Java plug-in, Java plugin, Java add-on or Java download
+        </p>
+        <p>
+            this is the para
+            Java software for your computer, or the Java Runtime Environment, is also referred to as the Java Runtime, Runtime Environment, Runtime, JRE, Java Virtual Machine, Virtual Machine, Java VM, JVM, VM, Java plug-in, Java plugin, Java add-on or Java download
+        </p>
+    
+    
+    `
+}
+
+function createTemplate(data){
+    var title=data.title; 
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+    var htmlTemplate=`
+        <html>
+            <head>
+                <title>
+                    S{title}
+                </title>
+                 <meta name="viewport" content="width=device-width,initial-scale=1"/>
+                 <link href="/ui/style.css" rel="stylesheet" />
+                
+            </head>
+            <body>
+                <div class="container">
+                <div>
+                      <a href= "/" >home</a>  
+                </div>
+                <hr/>
+                <h3>
+                    S{heading}
+                </h3>
+                
+                
+                <div>
+                    S{date}
+                </div>
+            
+               
+                <div>
+                    S{content}
+                </div>
+            </div>
+            </body>
+        </html>
+    
+    `;
+    return htmlTemplate;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -12,7 +95,7 @@ app.get('//best-background-pictures-for-laptop-hd-wallpaper.jpg', function (req,
   res.sendFile(path.join(__dirname, 'ui', 'best-background-pictures-for-laptop-hd-wallpaper.jpg'));
 });
 app.get('/article-one',function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article-three',function (req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
