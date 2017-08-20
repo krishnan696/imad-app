68d9e66dbd33e00d09cb200ac90145d2e9d14972
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-21 02:23:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,21 @@
 var button = document.getElementById('button');
-var counter=0;
 button.onclick=function(){
-    counter=counter+1;
-    var count=document.getElementById('count');
-    count.innerHTML=(counter.toString());
+var request= new XMLHttpRequest();
+request.onReadyStateChange = function()
+{
+  if(request.readyState== XMLHttpRequest.DONE)
+  {
+      if(request.status==200)
+      {
+          var counter=request.responseText();
+          counter=counter+1;
+          var count=document.getElementById('count');
+          count.innerHTML=(counter.toString());
+      }
+  }
+};
+request.open('GET', 'http://kris15226ec.imad.hasura-app.io/counter', True );
+request.send(NULL);
+
+   
 };
