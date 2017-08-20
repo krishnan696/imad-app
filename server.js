7962add08419568a9773adf0054b522cab32a652
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-20 17:02:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var articleOne=
     content:`
     
         <p>
-            this is the para
+            this is the 
             Java software for your computer, or the Java Runtime Environment, is also referred to as the Java Runtime, Runtime Environment, Runtime, JRE, Java Virtual Machine, Virtual Machine, Java VM, JVM, VM, Java plug-in, Java plugin, Java add-on or Java download
         </p>
     
