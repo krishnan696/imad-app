4f369acefb8d46a44f749d1afdf6c201a488a0ca
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-20 23:44:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,11 @@ var element =document.getElementById("main-text");
 element.innerHTML="java script";
 //move img to right
 var img= document.getElementById("img");
+var margin=0;
+function right(){
+    margin=margin+1;
+    img.style.marginLeft =margin+'px';
+}
 img.onclick = function() {
-    img.style.marginLeft =' 100px ' ;
+    var interval=setInterval( right,10) ;
 };
