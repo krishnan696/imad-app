37c597549fa49dab3c76261af814689409e95c18
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-22 01:59:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+//count in home page
 var button = document.getElementById('button');
 button.onclick=function(){
     var request= new XMLHttpRequest();
@@ -17,5 +18,20 @@ button.onclick=function(){
     request.open('GET', 'http://kris15226ec.imad.hasura-app.io/counter', true );
     request.send(null);         
 
+   
+};
+//list the given names
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var sumbit=document.getElementById('sumbit');
+submit.onclick= function(){
+   var names=['name1','name2','name3'];
+   var list='';
+   for(var i=0;i<names.length;i++)
+   {
+       list='<li>'+ names[i] +'</li>';
+   }
+   var ul=doucument.getElementById('list');
+   ul.innerElement=list;
    
 };
