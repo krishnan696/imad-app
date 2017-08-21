05987b7b35b07a099300890ae5e52156e316f152
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-22 02:19:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get('/counter',function(req,res){
 });
 var names=[];
 app.get('/submit-names/:name',function(req,res){
-    var names=re.params.name;
+    var names=res.params.name;
     names.push(names);
     res.send(JSON.stringify(names));
     
