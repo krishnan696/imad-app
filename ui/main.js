844e9300ad4f8046c644fcf364a54d687b1ff063
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-22 02:33:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,13 +25,28 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var sumbit=document.getElementById('sumbit');
 submit.onclick= function(){
-   var names=['name1','name2','name3'];
-   var list='';
-   for(var i=0;i<names.length;i++)
-   {
-       list +='<li>'+ names[i] +'</li>';
-   }
-   var ul=document.getElementById('list');
-   ul.innerHTML=list;
-   
+    var request= new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+      if(request.readyState===XMLHttpRequest.DONE)
+      {
+          if(request.status=== 200)
+          {
+               var name=request.responseText;
+               var names=[];
+               names.push(name);
+               var list='';
+           
+               for(var i=0;i<names.length;i++)
+               {
+                   list +='<li>'+ names[i] +'</li>';
+               }
+               var ul=document.getElementById('list');
+               ul.innerHTML=list;
+          }
+      }
+    }
+    request.open('GET', 'http://kris15226ec.imad.hasura-app.io/submit-names', true );
+    request.send(null); 
+  
 };
