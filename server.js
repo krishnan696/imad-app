270ea6c96b7a0d23f8db07c7f1efa4b514b7cd06
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-08-31 16:22:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.get('/test-tb',function(req,res)
       }
       else
       {
-          res.send(JSON.stringify(result));
+          res.send(JSON.stringify(result.rows));
       }
     });
     
