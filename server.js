a2516cd71ea79d2396645fa89fea8ad1f6104512
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-09-03 17:56:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var Crypto=require('crypto');
-var bodyParser=require('body-parser');
-
+var bodyParser = require('body-parser');
+var session=require('express-session');
 var config=
 {
     user:"kris15226ec",
@@ -18,7 +18,10 @@ var config=
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-
+app.use(session({
+    secret :"randomSecretValue",
+    cookie :{maxAge : 1000*60*60*24*30}
+}));
 function createTemplate(data){
     var title=data.title; 
     var heading=data.heading;
