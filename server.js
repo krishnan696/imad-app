96ed095dfb7bf09cd59d6a205877bbd0ac28d786
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-09-03 18:37:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -186,7 +186,7 @@ app.post('/create-user',function(req,res){
      }
  });
  app.get('/logout',function(req,res){
-     delete req.session.auth.userId;
+     delete req.session.auth;
      res.send("you are logout ");
  });
 app.get('/ui/style.css', function (req, res) {
