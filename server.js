a3cf509a2398a831b2d9a76729a6ef0f84786f28
krishnan696 <31028687+krishnan696@users.noreply.github.com>
2017-09-03 17:34:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/articles/:articleName',function (req, res){
 app.post('/create-user',function(req,res){
    var username=req.body.username;
    var password=req.body.password;
-   var salt=Crypto.getRandomBytes(128).toString('hex');
+   var salt=Crypto.randomBytes(128).toString('hex');
    var dbString=hash(password,salt);
    console.log(username);
    console.log(dbString);
